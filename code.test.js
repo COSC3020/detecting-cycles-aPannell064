22295ecbafcc2abe0d527b681575cbd4b60c431a
aPannell064 <adamkpannell@gmail.com>
2025-03-23 16:01:25 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -11,8 +11,12 @@ assert.strictEqual(hasCycle([[1], []]), false, "This graph should not have any c
 
 //Basic graph with a cycle
 assert.strictEqual(hasCycle([[1], [2], [0]]), true, "This graph should have a cycle");
-//jsc.assert(hasCycle([[1], [2], [0]]) == true);
 
 //More complex graph with no cycles
+assert.strictEqual(hasCycle([[1,3,5], [4], [3,5], [4], [], [6], []]), false, "This graph should not have any cycles");
 
 //More complex graph with cycles
+assert.strictEqual(hasCycle([[1,2,5], [4], [3,5], [4], [6], [6], [1]]), true, "This graph should have a cycle");
+
+//More complex undirected graph
+assert.strictEqual(hasCycle([[1,2,5], [0,4], [0,3,5], [2,4], [1,3], [0,2,6], [5]]), true, "This graph should have a cycke");
