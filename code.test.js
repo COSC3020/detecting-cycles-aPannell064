207dc3d885835fc371c24f051999d2d84d62f022
aPannell064 <adamkpannell@gmail.com>
2025-03-23 16:13:43 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -4,19 +4,19 @@ const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
 
 //Empty Graph
-assert.strictEqual(hasCycle([]), false, "Empty graph should not have cycles");
+assert.strictEqual(hasCycle([]), false);
 
 //Basic graph with no cycles
-assert.strictEqual(hasCycle([[1], []]), false, "This graph should not have any cycles");
+assert.strictEqual(hasCycle([[1], []]), false);
 
 //Basic graph with a cycle
-assert.strictEqual(hasCycle([[1], [2], [0]]), true, "This graph should have a cycle");
+assert.strictEqual(hasCycle([[1], [2], [0]]), true);
 
 //More complex graph with no cycles
-assert.strictEqual(hasCycle([[1,3,5], [4], [3,5], [4], [], [6], []]), false, "This graph should not have any cycles");
+assert.strictEqual(hasCycle([[1,3,5], [4], [3,5], [4], [], [6], []]), false);
 
 //More complex graph with cycles
-assert.strictEqual(hasCycle([[1,2,5], [4], [3,5], [4], [6], [6], [1]]), true, "This graph should have a cycle");
+assert.strictEqual(hasCycle([[1,2,5], [4], [3,5], [4], [6], [6], [1]]), true);
 
 //More complex undirected graph
-assert.strictEqual(hasCycle([[1,2,5], [0,4], [0,3,5], [2,4], [1,3], [0,2,6], [5]]), true, "This graph should have a cycle");
+assert.strictEqual(hasCycle([[1,2,5], [0,4], [0,3,5], [2,4], [1,3], [0,2,6], [5]]), true);
