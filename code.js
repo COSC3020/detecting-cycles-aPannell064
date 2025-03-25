a5088fdd4a91afa85e3fb6efd65841d55ac5f79c
aPannell064 <adamkpannell@gmail.com>
2025-03-24 18:41:59 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,10 @@
-function hasCycle(graph) {
+function hasCycle1(graph) {
     var toVisit;
     var current;
-    var copy;
+    var copy = JSON.parse(JSON.stringify(graph));
     //Try every start position
     for(var i = 0; i < graph.length; i++) {
         current = i;
-        copy = JSON.parse(JSON.stringify(graph));
         toVisit = [...copy[current]];
         copy[current] = []
 
@@ -22,3 +21,27 @@ function hasCycle(graph) {
     }  
     return false;
 }
+
+function hasCycle(graph) {
+    var visited = new Array(graph.length);
+    for (var startNode = 0; startNode < graph.length; startNode++) {
+        visited.fill(false);
+        if(dfSearch(graph, visited, startNode)) {return true;}
+    }
+    return false;  
+}
+
+function dfSearch(graph, visited, startNode) {
+    if (visited[startNode]) {return true;}
+    visited[startNode] = true;
+    var toCheck = [...graph[startNode]];
+
+    while(toCheck.length) { 
+        if(dfSearch(graph, visited, toCheck.shift())) {
+            return true;
+        }   
+    }
+    return false;
+}
+
+console.log(hasCycle([[], [0]]))
