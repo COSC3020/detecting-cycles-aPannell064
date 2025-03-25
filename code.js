abbc3a9878beaf6c3c4193044efa3dc6454b0d92
aPannell064 <adamkpannell@gmail.com>
2025-03-24 20:24:11 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,21 +1,33 @@
+
 function hasCycle(graph) {
+    //Array for all nodes that have been fully processed
+    var processed = new Array(graph.length).fill(false)
+    //Array for all nodes that are visited in each recurrsion stack
     var visited = new Array(graph.length).fill(false)
+    
     //Try each node as a start becasue we don't know where the graph starts
     for (var startNode = 0; startNode < graph.length; startNode++) {
-        if(dfTraversal(graph, visited, startNode)) {return true;}
+        if(dfTraversal(graph, visited, startNode, processed)) {return true;}
     }
     return false;  
 }
 
-function dfTraversal(graph, visited, startNode) {  
+function dfTraversal(graph, visited, startNode, processed) {
+    //If node has been completely proccessed already, return false
+    if(processed[startNode]) {return false;}
+    //If node has been visited, return true  
     if (visited[startNode]) {return true;}
     visited[startNode] = true;
+
     //Check each edge with a depth-first traversal
     var toCheck = [...graph[startNode]];
     while(toCheck.length) { 
-        if(dfTraversal(graph, visited, toCheck.shift())) {return true;}
+        if(dfTraversal(graph, visited, toCheck.shift(), processed)) {return true;}
     }
-    //Reset the visited for that node if the stack resolves
+
+    //Reset visited for that node since the stack resolved without returning true
     visited[startNode] = false;
+    //Mark node as completely processed
+    processed[startNode] = true;
     return false;
 }
--- code.test.js
+++ code.test.js
@@ -6,6 +6,9 @@ eval(fs.readFileSync('code.js')+'');
 //Empty Graph
 assert.strictEqual(hasCycle([]), false, "Failed on []");
 
+//Graph with one node
+assert.strictEqual(hasCycle([[]]), false, "Failed on [[]]");
+
 //Basic graph with no cycles
 assert.strictEqual(hasCycle([[1], []]), false, "Failed on [[1], []]");
 
@@ -17,6 +20,3 @@ assert.strictEqual(hasCycle([[1,2,5], [4], [3,5], [4], [], [6], []]), false, "Fa
 
 //More complex graph with cycles
 assert.strictEqual(hasCycle([[1,2,5], [4], [3,5], [4], [6], [6], [1]]), true, "Failed on [[[1,2,5], [4], [3,5], [4], [6], [6], [1]]");
-
-//More complex undirected graph
-assert.strictEqual(hasCycle([[1,2,5], [0,4], [0,3,5], [2,4], [1,3], [0,2,6], [5]]), true, "Failed on [[1,2,5], [0,4], [0,3,5], [2,4], [1,3], [0,2,6], [5]]");
